085dd18a67049fb09ecc5556405cc64ee8b689a4
rishnt <94091947+rishnt@users.noreply.github.com>
2025-09-22 21:51:38 +0530

    Added files

---
+++ src/Consumer.java
@@ -0,0 +1,6 @@
+public class Consumer implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Consumer is running ...");
+    }
+}
---
+++ src/Producer.java
@@ -0,0 +1,6 @@
+public class Producer implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Producer is running ...");
+    }
+}
